b082c2a037a926f631a92b2c7eab0b637dc9ca1b
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2021-10-11 10:52:42 -0500

    create a project

---
+++ app/src/main/java/com/miu/meditationapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.miu.meditationapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miu.meditationapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
