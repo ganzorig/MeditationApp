eb346401747324786039742b9d1c713064f1eb6a
egy <ba19d100@gmail.com>
2021-10-14 08:55:12 -0500

    Onboarding Task done

--- app/src/main/java/com/miu/meditationapp/MainActivity.kt
+++ app/src/main/java/com/miu/meditationapp/MainActivity.kt
@@ -1,19 +1,34 @@
 package com.miu.meditationapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.view.ViewParent
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContentProviderCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.view.plusAssign
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.button.MaterialButton
 import com.miu.meditationapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
+    private lateinit var indicatorContainer: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         setOnboardingItems()
+        setupIndicators()
+        setCurrentIndicator(0)
+
     }
     private fun setOnboardingItems(){
         onboardingItemsAdapter = OnboardingItemsAdapter(
@@ -32,5 +47,75 @@ class MainActivity : AppCompatActivity() {
         )
         val onboardingViewPager = findViewById<ViewPager2>(R.id.onboardingViewPager)
         onboardingViewPager.adapter = onboardingItemsAdapter
+        onboardingViewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(position)
+            }
+        })
+        (onboardingViewPager.getChildAt(0) as RecyclerView). overScrollMode =
+            RecyclerView.OVER_SCROLL_NEVER
+        findViewById<ImageView>(R.id.imageNext).setOnClickListener{
+            if (onboardingViewPager.currentItem+1 < onboardingItemsAdapter.itemCount){
+                onboardingViewPager.currentItem += 1
+            } else {
+                navigateToHomeActivity()
+            }
+        }
+        findViewById<TextView>(R.id.textSkip).setOnClickListener{
+            navigateToHomeActivity()
+        }
+        findViewById<MaterialButton>(R.id.buttonGetStarted).setOnClickListener{
+            navigateToHomeActivity()
+        }
+
+        }
+
+    private fun navigateToHomeActivity(){
+        startActivity(Intent(applicationContext,HomeActivity::class.java))
+        finish()
+    }
+
+
+    private fun setupIndicators(){
+        indicatorContainer = findViewById(R.id.indicatorsContainer)
+        val indicators = arrayOfNulls<ImageView>(onboardingItemsAdapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT )
+        layoutParams.setMargins(8,0,8,0,)
+        for (i  in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.let {
+                it.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active_background
+                    )
+                )
+                it.layoutParams = layoutParams
+                indicatorContainer.addView(it)
+            }
+        }
+    }
+    private fun setCurrentIndicator (position: Int){
+        val childCount = indicatorContainer.childCount
+        for( i  in 0 until childCount){
+            val imageView = indicatorContainer.getChildAt(i) as ImageView
+            if(i == position){
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active_background
+                    )
+                )
+            }else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_backgound
+                    )
+                )
+            }
+        }
     }
 }
