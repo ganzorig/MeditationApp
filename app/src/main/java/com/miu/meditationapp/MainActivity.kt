853d85c98f996f4db303cda282563a08e3e9a704
egy <ba19d100@gmail.com>
2021-10-13 21:55:59 -0500

    onboarding task

--- app/src/main/java/com/miu/meditationapp/MainActivity.kt
+++ app/src/main/java/com/miu/meditationapp/MainActivity.kt
@@ -2,17 +2,35 @@ package com.miu.meditationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewParent
+import androidx.viewpager2.widget.ViewPager2
 import com.miu.meditationapp.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var onboardingItemsAdapter: OnboardingItemsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
+        setOnboardingItems()
+    }
+    private fun setOnboardingItems(){
+        onboardingItemsAdapter = OnboardingItemsAdapter(
+            listOf(
+                OnboardingItem(
+                    onboardingImage = R.drawable.pic1,
+                    title = "Welcome to Meditation",
+                    description = "Meditation is a practice where an individual uses a technique."
+                ),
+                OnboardingItem(
+                    onboardingImage = R.drawable.pic3,
+                    title = "Let's meditate",
+                    description = "Meditation is practiced in numerous religious traditions. "
+                )
+            )
+        )
+        val onboardingViewPager = findViewById<ViewPager2>(R.id.onboardingViewPager)
+        onboardingViewPager.adapter = onboardingItemsAdapter
     }
 }
