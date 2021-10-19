7e5f16f592ee3ef53a68608b3f6337db180a7dc2
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2021-10-19 00:36:13 -0500

    add breathe exercise

--- app/src/main/java/com/miu/meditationapp/BreathActivity.kt
+++ app/src/main/java/com/miu/meditationapp/BreathActivity.kt
@@ -1,11 +1,13 @@
 package com.miu.meditationapp
 
+import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.transition.Visibility
 import android.view.View
+import android.widget.Toast
+import com.airbnb.lottie.LottieAnimationView
 import kotlinx.android.synthetic.main.activity_breath.*
-import kotlinx.android.synthetic.main.fragment_home.*
 
 class BreathActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,14 +16,23 @@ class BreathActivity : AppCompatActivity() {
 
         play.setOnClickListener {
             play.playAnimation()
-
-            //breatheView.visibility = View.VISIBLE
-            //play.visibility = View.GONE
-
-
         }
 
-//
-//        play.addAnimatorListener()
+        play.addAnimatorListener(object : Animator.AnimatorListener{
+            override fun onAnimationRepeat(animation: Animator?) {
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                breatheView.visibility = View.VISIBLE
+                play.visibility = View.GONE
+                Toast.makeText(applicationContext, "sad", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+            }
+        })
     }
 }
--- app/src/main/java/com/miu/meditationapp/MeditationActivity.kt
+++ app/src/main/java/com/miu/meditationapp/MeditationActivity.kt
@@ -9,10 +9,14 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.speech.tts.TextToSpeech
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.miu.meditationapp.databinding.ActivityMeditationBinding
 import kotlinx.android.synthetic.main.activity_meditation.*
+import java.text.DecimalFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -24,6 +28,9 @@ class MeditationActivity : AppCompatActivity() {
     private var isFullscreen: Boolean = false
     private lateinit var tts: TextToSpeech
     private lateinit var  values : Array<String>
+    private var minutes = 20L
+    private lateinit var textIndicator:TextView
+    private lateinit var timer: CountDownTimer
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,36 +41,27 @@ class MeditationActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         isFullscreen = true
 
+        textIndicator = indicator
+
         values = resources.getStringArray(R.array.minutes);
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, values)
         spinner.adapter = adapter;
 
-        val textIndicator = indicator
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                minutes = if(position == 0) 20L else 10L
+                textIndicator.text = "$minutes:00"
+                timer.cancel()
+                timer = createTimer()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
+
         mediaPlayer = MediaPlayer.create(applicationContext, R.raw.back_sound)
 
-        val timer = object: CountDownTimer(1200000, 1000) {
-            var min = 20L
-            var sec = 0L
-            override fun onTick(ms: Long) {
-                min = TimeUnit.MILLISECONDS.toMinutes(ms) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(ms))
-                sec = TimeUnit.MILLISECONDS.toSeconds(ms) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(ms))
-
-                if(min == 19L && sec == 55L) {
-                    tts = TextToSpeech(applicationContext, TextToSpeech.OnInitListener {
-                        if(it == TextToSpeech.SUCCESS) {
-                            tts.language = Locale.US
-                            tts.setSpeechRate(0.8F)
-                            tts.speak("Close your eyes and sit comfortably", TextToSpeech.QUEUE_ADD, null)
-                        }
-                    })
-                }
-                textIndicator.text = "$min:$sec"
-            }
-
-            override fun onFinish() {
-                println("finish")
-            }
-        }
+        timer = createTimer()
 
         close.setOnClickListener {
             showDialog(this)
@@ -72,8 +70,12 @@ class MeditationActivity : AppCompatActivity() {
         start.setOnClickListener {
             timer.start()
 
+            spinner.isEnabled = false
+            spinner.isClickable = false
             mediaPlayer.isLooping = true
             mediaPlayer.start()
+
+            start.isClickable = false
             start.text = getString(R.string.str_end)
         }
 
@@ -88,7 +90,6 @@ class MeditationActivity : AppCompatActivity() {
         }
     }
 
-
     override fun onStop() {
         super.onStop()
         if (mediaPlayer.isPlaying) {
@@ -99,6 +100,31 @@ class MeditationActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         showDialog(this)
+    }
+
+    private fun createTimer(): CountDownTimer {
+        return object: CountDownTimer(minutes * 60000, 1000) {
+            var sec = 0L
+            override fun onTick(ms: Long) {
+                minutes = TimeUnit.MILLISECONDS.toMinutes(ms) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(ms))
+                sec = TimeUnit.MILLISECONDS.toSeconds(ms) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(ms))
+
+                if((minutes == 19L || minutes == 0L) && sec == 55L) {
+                    tts = TextToSpeech(applicationContext, TextToSpeech.OnInitListener {
+                        if(it == TextToSpeech.SUCCESS) {
+                            tts.language = Locale.US
+                            tts.setSpeechRate(0.8F)
+                            tts.speak("Close your eyes and sit comfortably", TextToSpeech.QUEUE_ADD, null)
+                        }
+                    })
+                }
+                textIndicator.text = "$minutes:$sec"
+            }
+
+            override fun onFinish() {
+                println("finish")
+            }
+        }
     }
 
     private fun showDialog(context: Context){
