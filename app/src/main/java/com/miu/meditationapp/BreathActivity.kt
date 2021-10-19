2e3bc2f420630f5a291c43f55c8c93b7f22d6679
Ganzorig Bayarsaikhan <ganzorig.b@nmma.co>
2021-10-19 01:36:29 -0500

    Add breathe exercise. close #2

--- app/src/main/java/com/miu/meditationapp/BreathActivity.kt
+++ app/src/main/java/com/miu/meditationapp/BreathActivity.kt
@@ -3,16 +3,16 @@ package com.miu.meditationapp
 import android.animation.Animator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.transition.Visibility
 import android.view.View
 import android.widget.Toast
-import com.airbnb.lottie.LottieAnimationView
 import kotlinx.android.synthetic.main.activity_breath.*
 
 class BreathActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_breath)
+
+        breathe.visibility = View.GONE
 
         play.setOnClickListener {
             play.playAnimation()
@@ -23,7 +23,7 @@ class BreathActivity : AppCompatActivity() {
             }
 
             override fun onAnimationEnd(animation: Animator?) {
-                breatheView.visibility = View.VISIBLE
+                breathe.visibility = View.VISIBLE
                 play.visibility = View.GONE
                 Toast.makeText(applicationContext, "sad", Toast.LENGTH_SHORT).show()
             }
