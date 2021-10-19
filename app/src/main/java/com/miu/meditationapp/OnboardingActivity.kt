b1375f763a30964ede8bdb9ec5fc1cc5d8ea2795
egy <ba19d100@gmail.com>
2021-10-19 12:06:23 -0500

    Add onboarding text. close #6

--- app/src/main/java/com/miu/meditationapp/OnboardingActivity.kt
+++ app/src/main/java/com/miu/meditationapp/OnboardingActivity.kt
@@ -3,6 +3,7 @@ package com.miu.meditationapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
 import android.widget.LinearLayout
@@ -30,27 +31,27 @@ class OnboardingActivity : AppCompatActivity() {
         onboardingItemsAdapter = OnboardingItemsAdapter(
             listOf(
                 OnboardingItem(
-                    onboardingImage = R.drawable.img_hellowe,
+                    onboardingImage = R.drawable.img_hello,
                     title = "Welcome to GENO",
                     description = "We'll show you how to train your mind for happier, healthier life."
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.img_hi5,
+                    onboardingImage = R.drawable.img_medi,
                     title = "Meditation made simple!",
                     description = "It's okay if you've never meditated before"
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.img_breathe,
+                    onboardingImage = R.drawable.img_book,
                     title = "Breathe",
                     description = "We'll guide you through ..."
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.img_community,
+                    onboardingImage = R.drawable.img_communi,
                     title = "Community",
                     description = "You can reach out to anyone at anytime."
                 ),
                 OnboardingItem(
-                    onboardingImage = R.drawable.img_inspired,
+                    onboardingImage = R.drawable.img_paint,
                     title = "Get inspired",
                     description = "In fact, we made a quick video to help you learn the basics"
                 ),
@@ -117,6 +118,10 @@ class OnboardingActivity : AppCompatActivity() {
         val childCount = indicatorContainer.childCount
         for( i  in 0 until childCount){
             val imageView = indicatorContainer.getChildAt(i) as ImageView
+            imageView.scaleType = ImageView.ScaleType.FIT_END
+            val layoutParams: LinearLayout.LayoutParams =
+                LinearLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT )
+            imageView.layoutParams = layoutParams
             if(i == position){
                 imageView.setImageDrawable(
                     ContextCompat.getDrawable(
